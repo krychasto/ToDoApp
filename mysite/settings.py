834213b15b703abd302a7fce3c58bcd6c11643de
krychasto <krystian.freus@wp.pl>
2021-03-04 09:03:31 +0100

    Update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,9 +25,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['krychasto-todoapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
