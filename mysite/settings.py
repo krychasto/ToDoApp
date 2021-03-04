21c2f2bcc81c0601f6cb37b3a5578d55d010ec83
krychasto <krystian.freus@wp.pl>
2021-03-04 09:04:54 +0100

    Debug=False

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['krychasto-todoapp.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-		'todo',
+	'todo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
